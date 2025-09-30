0e5fdb627b6265a9db85d8b35bbbaebe8b0b6445
saul <saul.santoa@gmail.com>
2025-09-30 14:36:28 +0700

    fix: railway configuration for laravel deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'api.key' => \App\Http\Middleware\ApiKeyMiddleware::class,
         ]);
+
+        
+         $middleware->append(\App\Http\Middleware\ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

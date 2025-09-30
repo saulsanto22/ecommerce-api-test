414244d81e948817f0a89a73fec96277cb1d000f
saul <saul.santoa@gmail.com>
2025-09-30 13:26:01 +0700

    fix: railway configuration for laravel deployment

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -91,7 +91,7 @@ return [
             /*
              * Edit to set the api's base path
              */
-            'base' => env('L5_SWAGGER_BASE_PATH', null),
+           'base' => env('APP_URL') . '/docs',
 
             /*
              * Absolute path to directories that should be excluded from scanning
